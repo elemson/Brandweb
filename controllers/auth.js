f1b43c34d07f4044f6a1d4cf4c8b224f10912946
elemson <toluwhani@gmail.com>
2020-05-13 17:02:44 +0300

    added error handler middleware and error response utility function. Created the user model

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+const User = require("../models/User");
+const asyncHandler = require("../middleware/async");
+const ErrorResponse = require("../utils/errorResponse");
+
+// @desc Register user
+// @route GET /api/v1/user/register
+// @route Public
+
+exports.register = asyncHandler(async (req, res, next) => {
+  res.status(200).json({ success: true });
+});
--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const morgan = require("morgan");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to database
+connectDB();
+
+//Route Files
 const app = express();
 
+app.use(express.json());
+// app.get("/api/v1/bootcamps", (req, res) => {
+//   res.status(200).json({ success: true, msg: "show all" });
+// });
+
+//Dev logging middleware
+if (process.env.NODE_ENV === "develoment") {
+  app.use(morgan("dev"));
+}
+
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
-app.listen(
+
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
 );
 
-server.listen(PORT, () => {
-  console.log(`listening on port, ${PORT}`);
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  //Close sever & exit proess
+  server.close(() => process.exit(1));
 });
