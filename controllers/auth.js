f4002f38723e26b13ee3fd84b9e173875ed46ad5
elemson <toluwhani@gmail.com>
2020-05-14 15:50:03 +0300

    Forgot password and generate token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const asyncHandler = require("../middleware/async");
+const sendEmail = require("../utils/sendEmail");
 const ErrorResponse = require("../utils/errorResponse");
 
 // @desc Register user
@@ -92,6 +93,32 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   //Get reset token
   const resetToken = user.getResetPasswordToken();
   await user.save({ validateBeforeSave: false });
+
+  //Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you have requested the reset of a password. Please clock ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Password reset token",
+      message,
+    });
+
+    res.status(200).json({ success: true, data: "Email sent" });
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse("Email could not be sent", 500));
+  }
+
   res.status(200).json({
     success: true,
     data: user,
