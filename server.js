deeb56ef46cace018e1ff07bf2c7f0faa93744be
elemson <toluwhani@gmail.com>
2020-05-15 15:30:11 +0300

    request rate limiting. HPP. Allow Cors

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const mongoSanitize = require("express-moongo-sanitize");
+const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
+const hpp = require("hpp");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const xss = require("xss-clean");
 const errorHandler = require("./middleware/error");
 
 const connectDB = require("./config/db");
@@ -38,6 +42,23 @@ app.use(mongoSanitize());
 
 //set security headers
 app.use(helmet());
+
+// Prevent XSS attcks
+app.use(xss());
+
+//Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10mins
+  max: 100,
+});
+
+app.use(limiter);
+
+//Prevent http param poluution
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
 
 //Mount routers
 app.use("/api/v1/auth", auth);
