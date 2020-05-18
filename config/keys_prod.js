d6508dcf9d20293993387428c30e53dbb973b812
elemson <toluwhani@gmail.com>
2020-05-18 04:51:41 +0300

    start corrected script typo error

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,7 +2,7 @@ module.exports = {
 NODE_ENV: process.env.NODE_ENV,
 PORT=process.env.PORT,
 
-MONGO_URI : process.env,MONGO_URI,
+MONGO_URI : process.env.MONGO_URI,
 
 JWT_SECRET:process.env.JWT_SECRET,
 JWT_EXPIRE:process.env.JWT_EXPIRE,
